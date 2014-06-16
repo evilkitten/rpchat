bfcd7462a0ee4936a1693f5eab07ceaddc6888d3
James Reeves <jreeves@weavejester.com>
2014-06-16 17:32:37 +0100

    Add hogan.js (mustache) view engine

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var engines = require('consolidate');
 
 var app = express();
 
+// Setup template engine
+app.engine('html', engines.hogan);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
 app.get("/", function(req, res) {
-  res.send("Hello World");
+  res.render('index');
 });
 
 var server = app.listen(8000, function() {
